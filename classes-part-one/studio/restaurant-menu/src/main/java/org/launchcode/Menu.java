fe32c470aa5388b3c927916051825f960dd19a5c
mll0032 <mllawson18@gmail.com>
2024-09-14 19:33:50 -0500

    Creating Classes for Menu and MenuItem

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,32 @@
 package org.launchcode;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Menu {
+    private ArrayList<MenuItem> items;
+    private Date lastUpdated;
+
+    public Menu() {
+        items = new ArrayList<>();
+        lastUpdated = new Date();
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+        lastUpdated = new Date();
+    }
+
+    public void removeItem(MenuItem item) {
+        items.remove(item);
+        lastUpdated = new Date();
+    }
+
+    public void displayMenu() {
+        for (MenuItem item : items) {
+            System.out.println(item);
+        }
+    }
 }
+
+
+
